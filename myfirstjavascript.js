c4c297dd0707c06ee84653b3966d7b7715871942
Nishant Singh <755083@student.tor.loc>
2019-05-22 16:40:45 -0400

    4:40

--- myfirstjavascript.js
+++ myfirstjavascript.js
@@ -1,7 +1,10 @@
 // week 3 in class work//
 
-funtion subtract(num1, num2) {
+function substract(num1, num2) {
 
     return num1 - num2;
 
+
 }
+substract(7, 2);
+alert("the answer is " + substract(7, 2));
